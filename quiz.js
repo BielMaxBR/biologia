ef6067fc6a654321e3ae8c1bde524b71f504cb10
Gabriel Maximo <gbielmaximo@gmail.com>
2020-06-13 19:01:44 +0000

    tá mais perto do que parece

---
+++ quiz.js
@@ -0,0 +1,18 @@
+var url   = window.location.search.replace("?", "");
+var dados = url.split("&");
+
+var hash = {}
+for(var i = 0; i < dados.length; i++){
+    var parametro = dados[i].split("=");
+    var chave = parametro[0];
+    var valor = parametro[1];
+    hash[chave] = valor;
+}
+console.log(hash)
+
+
+notaH1 = document.getElementById('nota')
+
+if (notaH1) {
+    notaH1.innerHTML = 'Você tirou ' + nota.toString() + ' de 10!'
+}
